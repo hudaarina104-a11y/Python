a25b00a440e973c68b5d8be1719faafd136747fd
Yaryna65 <hudaarina627@gmail.com>
2026-09-03 09:46:15 +0300

    calculator

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello, world")
+# Розробник: Худа Ярина
+
+def run_calculator():
+    try:
+        num1 = float(input("Введіть перше число: "))
+        operator = input("Введіть операцію (+, -, *, /): ")
+        num2 = float(input("Введіть друге число: "))
+
+        if operator == '+':
+            result = num1 + num2
+        elif operator == '-':
+            result = num1 - num2
+        elif operator == '*':
+            result = num1 * num2
+        elif operator == '/':
+            if num2 == 0:
+                return "Помилка: ділення на нуль заборонено!"
+            result = num1 / num2
+        else:
+            return "Помилка: невідомий оператор!"
+        
+        return f"Результат: {num1} {operator} {num2} = {result}"
+        
+    except ValueError:
+        return "Помилка: потрібно вводити саме числа!"
+
+print(run_calculator())
